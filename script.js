2290074b6c1585d88be42396c9038f486096d79e
AshVosem <ash.vosem@gmail.com>
2020-03-19 01:53:20 +0300

    feat: update slider

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+// flags
+
 const nav = document.getElementById('nav');
 
 nav.addEventListener('click', (event) => {
     nav.querySelectorAll('a').forEach(element => element.classList.remove('active'));
     event.target.classList.add('active'); 
 });
+
+// flags scroll
+
+document.addEventListener('scroll', (event) => {
+    const scroll = window.scrollY;
+    const flags = document.querySelectorAll('main>section');
+    console.log(flags);
+});
+
 
 const wallpaper_v = document.getElementById('layer_v');
 const wallpaper_h = document.getElementById('layer_h');
@@ -23,6 +34,8 @@ document.getElementById('square_h').addEventListener('click', () => {
     }
 });
 
+// slider
+
 const slides = document.querySelectorAll('#slides .slide');
 const sliderSection = document.getElementById('slider');
 const chev_l = document.getElementById('chev_l');
@@ -35,14 +48,14 @@ chev_l.addEventListener('click', () => {
     if(slides[activeSlide].classList.contains('slide_1')) { 
         sliderSection.style.backgroundColor = '#648bf0';
         sliderSection.style.borderBottomColor = '#adc5db';
-        slides[activeSlide].classList.remove('active_slide');
-        slides[activeSlide - 1].classList.add('active_slide');
+        slides[activeSlide].classList.remove('active_slide_l');
+        slides[activeSlide - 1].classList.add('active_slide_r');
     } 
     else { 
         sliderSection.style.backgroundColor = '#f06c64';
         sliderSection.style.borderBottomColor = '#ea676b';
-        slides[activeSlide].classList.remove('active_slide');
-        slides[activeSlide + 1].classList.add('active_slide');
+        slides[activeSlide].classList.remove('active_slide_r');
+        slides[activeSlide + 1].classList.add('active_slide_l');
     }
 })
 chev_r.addEventListener('click', () => {
@@ -50,20 +63,23 @@ chev_r.addEventListener('click', () => {
     if(slides[activeSlide].classList.contains('slide_1')) { 
         sliderSection.style.backgroundColor = '#648bf0';
         sliderSection.style.borderBottomColor = '#96c5f2';
-        slides[activeSlide].classList.remove('active_slide');
-        slides[activeSlide - 1].classList.add('active_slide');
+        slides[activeSlide].classList.remove('active_slide_l');
+        slides[activeSlide - 1].classList.add('active_slide_r');
     } 
     else { 
         sliderSection.style.backgroundColor = '#f06c64';
         sliderSection.style.borderBottomColor = '#ea676b';
-        slides[activeSlide].classList.remove('active_slide');
-        slides[activeSlide + 1].classList.add('active_slide');
+        slides[activeSlide].classList.remove('active_slide_r');
+        slides[activeSlide + 1].classList.add('active_slide_l');
     }
 })
 
 let portfolio_pics = document.getElementById('portfolio_pics');
 
 let pics = [];
+
+//portfolio pics border
+
 Array.from(document.getElementById('portfolio_pics').querySelectorAll('div')).forEach(element => pics.push(`${element.classList}`));
 
 portfolio_pics.addEventListener('click', (event) => {
@@ -72,6 +88,7 @@ portfolio_pics.addEventListener('click', (event) => {
 });
 
 // iq over 100000000 sorry ))0)
+// random pics in portfolio
 
 portfolio_nav.addEventListener('click', (event) => {
     portfolio_nav.querySelectorAll('p').forEach(element => element.classList.remove('portfolio_active'));
@@ -81,7 +98,7 @@ portfolio_nav.addEventListener('click', (event) => {
         return Math.random() - 0.5 
     })
 
-    portfolio_pics.querySelectorAll('div').forEach((elem,index) => elem.classList.remove(`${elem.classList.item(0)}`));
+    portfolio_pics.querySelectorAll('div').forEach((elem) => elem.classList.remove(`${elem.classList.item(0)}`));
     portfolio_pics.querySelectorAll('div').forEach((elem,index) => elem.classList.add(`${pics[index]}`));
 });
 
